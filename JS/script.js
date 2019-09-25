b6da2ae34c260dde2b07e877f5096eb344340692
Danas Mikelionis Linux <danas.mikelionis@gmail.com>
2019-09-25 18:41:30 +0300

    Added Google sign in and out buttons(with the help of tutorials)

---
+++ JS/script.js
@@ -0,0 +1,27 @@
+function onSignIn(googleUser) {
+        // Useful data for your client-side scripts:
+        var profile = googleUser.getBasicProfile();
+        console.log('Full Name: ' + profile.getName());
+        console.log("Image URL: " + profile.getImageUrl());
+        console.log("Email: " + profile.getEmail());
+
+        // The ID token you need to pass to your backend:
+        var id_token = googleUser.getAuthResponse().id_token;
+        console.log("ID Token: " + id_token);
+      }
+
+let signOutButtons = document.getElementsByClassName('sign-out-button');
+addSignOutButtonsAction();
+
+function addSignOutButtonsAction() {
+	for (let i = 0; i < signOutButtons.length; i++)
+		{
+			signOutButtons[i].addEventListener
+				('click', function() {
+					var auth2 = gapi.auth2.getAuthInstance();
+				        auth2.signOut().then(function () {
+     				        console.log('User signed out.');
+  			       		 });
+				});
+		}
+}
